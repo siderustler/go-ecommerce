a72a79b27692eb1e37d58a27ebd74df506418a15
siderustler <m.lojewski1998@gmail.com>
2025-09-22 19:28:31 +0200

    add gofiber, fragment and template rendering helper fns

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/siderustler/go-ecommerce/views"
+)
+
+func main() {
+	f := fiber.New()
+	f.Get("/details", func(c *fiber.Ctx) error {
+		if c.Query("fragment") == "xd" {
+			RenderFragment(c, views.ProductDetails(), os.Stdout)
+			os.Stdout.Write([]byte("rendering"))
+			return RenderFragment(c, views.ProductDetails(), views.DetailFragment)
+		}
+		return Render(c, views.ProductDetails())
+	})
+	f.Listen(":8080")
+}
+
+func Render(c *fiber.Ctx, component templ.Component) error {
+	c.Set("Content-Type", "text/html")
+	return component.Render(c.Context(), c.Response().BodyWriter())
+}
+
+func RenderFragment(c *fiber.Ctx, component templ.Component, ids any) error {
+	c.Set("Content-Type", "text/html")
+	return templ.RenderFragments(c.Context(), c.Response().BodyWriter(), component, ids)
 }
