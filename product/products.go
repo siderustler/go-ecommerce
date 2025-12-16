1824428bb58708556f0aa275dbf930c493bf0bae
siderustler <m.lojewski1998@gmail.com>
2025-12-16 17:52:03 +0100

    refactor: remove dead code

--- product/products.go
+++ product/products.go
@@ -11,11 +11,3 @@ func (s Services) Products(ctx context.Context, page int, filter Filter) ([]Prod
 		NewProduct("3", "trzy", "/public/products/essa/1.webp", 3.99),
 	}, nil
 }
-
-func (s Services) ProductsByID(ids []string) ([]Product, error) {
-	return []Product{
-		NewProduct("1", "essa", "/public/products/essa/1.webp", 1.99),
-		NewProduct("2", "dwa", "/public/products/essa/1.webp", 2.99),
-		NewProduct("3", "trzy", "/public/products/essa/1.webp", 3.99),
-	}, nil
-}
